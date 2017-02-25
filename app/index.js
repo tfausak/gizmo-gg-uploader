893657cd523dff16171922ec3e9de69565ecb9bc
Taylor Fausak <taylor@fausak.me>
2017-02-25 07:42:15 -0600

    Watch the replay directory for new files

--- app/index.js
+++ app/index.js
@@ -2,6 +2,36 @@
 
 'use strict';
 
+const chokidar = require('chokidar');
 const electron = require('electron');
+const os = require('os');
+const path = require('path');
 
-document.title = `gizmo.gg uploader v${electron.remote.app.getVersion()}`;
+const version = electron.remote.app.getVersion();
+let replayDirectory = process.env.GIZMO_REPLAY_DIR;
+const replayPattern = /\.replay$/i;
+
+if (!replayDirectory) {
+  const home = os.homedir();
+  const demos = path.join('Rocket League', 'TAGame', 'Demos');
+
+  switch (os.platform()) {
+  case 'darwin':
+    replayDirectory = path.join(home, 'Library', 'Application Support', demos);
+    break;
+  case 'win32':
+    replayDirectory = path.join(home, 'Documents', 'My Games', demos);
+    break;
+  default:
+    replayDirectory = path.join(home, '.local', 'share', demos);
+  }
+}
+
+document.title = `gizmo.gg uploader ${version}`;
+
+chokidar.watch(replayDirectory, {ignoreInitial: true}).on('add', (file) => {
+  if (!replayPattern.test(file)) {
+    return;
+  }
+  console.log(file);
+});
